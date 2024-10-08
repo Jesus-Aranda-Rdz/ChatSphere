1ac74eb543a6d2b9a864d8a10523565d280c3bfc
Jesus-Aranda-Rdz <jesus_aranda_rodriguez@hotmail.com>
2024-10-07 21:15:53 -0600

    [ADD]Added Tailwind CSS to the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}', // Configura para buscar en archivos HTML y TS dentro de /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
